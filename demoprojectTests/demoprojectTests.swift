da7810a98c0a931ebc23d539e863437b54aaeea6
Richmond Asiedu <asiedurichmond713@gmail.com>
2025-05-10 15:38:18 -0400

    Added prework files to repository

---
+++ demoprojectTests/demoprojectTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoprojectTests.swift
+//  demoprojectTests
+//
+//  Created by Richmond Asiedu on 5/10/25.
+//
+
+import Testing
+@testable import demoproject
+
+struct demoprojectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
